e15bdbe9318f2b8d8dcd44ffdcda19b2739e840b
Lakshay Varshney <Lv1671>
2023-09-21 23:43:02 +0530

    two_sum

--- leetcode/MoveZeros.java
+++ leetcode/MoveZeros.java
@@ -1,27 +1,24 @@
 
-import java.util.*;
-public class Main{
-    public static Scanner scn = new Scanner(System.in);
-    public static void main(String[] args){
-        int arr [] = {0,1,3,0,12,0,7,0,9};
-        int i = 0;
-        int j = 0;
-        
-        while( j < arr.length){
-            
-            if(arr[j] != 0 && arr[i] == 0){
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-            }
-            if(arr[i] != 0){
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] == 0) {
+                j++;
+                }else{
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
                 i++;
+                j++;
             }
-            j++;
+            
         }
-        
-        for(int val : arr){
-            System.out.println(val);
-        }
+    }
+
+    public void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+      
+      moveZeroes(nums);
     }
 }
